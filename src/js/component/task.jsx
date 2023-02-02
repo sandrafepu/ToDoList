c79228f2a415f7decd7dfd4450e9bdc0d82359bf
sandrafepu <sandrafepu@gmail.com>
2023-02-02 20:05:25 +0000

    Arreglo tarea eliminar

--- src/js/component/task.jsx
+++ src/js/component/task.jsx
@@ -4,12 +4,26 @@ const Task = (props) => {
 
 
     const handleClick = (event) => {
-        props.removeTask(event.target.id)
+        props.removeTask(event.target.id)    
     }
 
+    const handleMouseEnter = (e) =>{
+        e.target.children[0].classList.replace("hide", "show")
+        e.target.children[0].classList.add("rotate-vert-center")
+       }
+       const handleMouseLeave = (e) =>{
+        e.target.children[0].classList.replace("show", "hide")
+        e.target.children[0].classList.remove("rotate-vert-center")
+       }
+    
     return(
-        <li className="task-line">
-            {props.taskName} <button className="button-close" type="button" onClick={handleClick} id={props.taskIndex} class="btn-close"></button>
+        <li className="task-line"
+        onMouseEnter={(e)=>handleMouseEnter(e)}
+        onMouseLeave={(e)=>handleMouseLeave(e)}
+        >
+            
+            {props.taskName} < button className="button-close " type="button"  onClick={handleClick}
+             id={props.taskIndex} class={"hide btn-close"}></button> 
         </li>
     )
 
